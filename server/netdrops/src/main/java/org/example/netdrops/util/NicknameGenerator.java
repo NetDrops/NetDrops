173bbd30debca3a064c6a6fe0e07e56b7fc50f9a
choijw1004 <97choijw@gmail.com>
2025-04-10 00:27:02 +0900

    feat: modify nickNameGenerator.java

--- server/netdrops/src/main/java/org/example/netdrops/util/NicknameGenerator.java
+++ server/netdrops/src/main/java/org/example/netdrops/util/NicknameGenerator.java
@@ -1,16 +1,11 @@
 package org.example.netdrops.util;
-
-import java.util.List;
-import java.util.Random;
-
+import java.util.UUID;
 public class NicknameGenerator {
-    private static final List<String> ADJECTIVES = List.of("익명", "귀여운", "멋진", "용감한", "조용한");
-    private static final List<String> ANIMALS = List.of("토끼", "고양이", "강아지", "사슴", "너구리", "곰", "다람쥐");
-    private static final Random RANDOM = new Random();
 
     public static String generate() {
-        String adjective = ADJECTIVES.get(RANDOM.nextInt(ADJECTIVES.size()));
-        String animal = ANIMALS.get(RANDOM.nextInt(ANIMALS.size()));
-        return adjective + " " + animal;
+        String nickName = "익명_" + UUID.randomUUID().toString().substring(0, 5);
+        return nickName;
     }
 }
+
+
